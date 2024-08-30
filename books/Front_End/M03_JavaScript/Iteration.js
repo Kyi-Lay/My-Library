7ffc5cb8e0a787387c1bf73fe4dcf67c8ae83713
pannajota <jotapanna2014@gmail.com>
2024-08-30 13:33:35 +0630

    commit 01 conditional stament commit

---
+++ books/Front_End/M03_JavaScript/Iteration.js
@@ -0,0 +1,7 @@
+// TODO: Write Your JavaScript Code Here
+let students = ["lulu", "SuSu", "lili", "TunTun", "koko", "LinLin",];
+console.log(students.length);
+
+for (let i = 0; i < students.length; i++) {
+    console.log("Welcome. Nice to see you, ", students[i], "!");
+}
